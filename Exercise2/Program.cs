e5335dea87f56060d87b5b66c8c115a6a13fb7ab
Danah Aker <danah20002010@gmail.com>
2022-01-16 19:33:01 +0200

    first commit

---
+++ Exercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var query = new List<int> { 10, 20, 30 }.Transform(x => x / 10);
+        foreach(var value in query)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
+
+
+    public delegate int TransformationFunction(int input);
+
+    public static class Transformation
+    {
+        public static IEnumerable<int> Transform(this IEnumerable<int> input, TransformationFunction transformationFunction)
+        {
+            foreach (var value in input)
+            {
+                yield return transformationFunction(value);
+            }
+        }
+    }
+
+
+
